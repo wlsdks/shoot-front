4dcfe11b2fc88476abc3ad93e8507610bfae2894
wlsdks <wlsdks12@naver.com>
2025-05-10 19:52:03 +0900

    :sparkles: api에 tan stack query 적용

--- src/features/profile/model/hooks/useProfile.ts
+++ src/features/profile/model/hooks/useProfile.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateUserStatus } from '../../api/profile';
+import { setProfileImage, setBackgroundImage } from '../../api/profile';
 
 export const useProfile = (userId: number) => {
     const queryClient = useQueryClient();
@@ -13,9 +14,31 @@ export const useProfile = (userId: number) => {
         },
     });
 
+    // 프로필 이미지 설정
+    const updateProfileImage = useMutation({
+        mutationFn: setProfileImage,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['currentUser'] });
+        },
+    });
+
+    // 배경 이미지 설정
+    const updateBackgroundImage = useMutation({
+        mutationFn: setBackgroundImage,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['currentUser'] });
+        },
+    });
+
     return {
         updateStatus: updateStatus.mutate,
         isUpdating: updateStatus.isPending,
         updateError: updateStatus.error,
+        updateProfileImage: updateProfileImage.mutate,
+        isUpdatingProfileImage: updateProfileImage.isPending,
+        updateProfileImageError: updateProfileImage.error,
+        updateBackgroundImage: updateBackgroundImage.mutate,
+        isUpdatingBackgroundImage: updateBackgroundImage.isPending,
+        updateBackgroundImageError: updateBackgroundImage.error,
     };
 }; 
